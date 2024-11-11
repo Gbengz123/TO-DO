d63552140cf20042c0c3ed6f9db6d82766a77435
Daniel <danielgbenga11@gmail.com>
2024-11-11 09:27:28 +0100

    Dynamically create inbox page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './style.css';
+import inboxPageLoad from './inbox';
+
+const InboxBtn = document.querySelector('#InboxBtn')
+let currentPage =''
+
+InboxBtn.addEventListener('click', () => {
+    
+    if (currentPage ==! 'inboxPage') {
+        currentPage = inboxPageLoad()
+    }
+})
+
 
 const formModal = document.querySelector('#modalDialog');
 const formClosebtn = document.querySelector('#closeModal');
@@ -12,26 +24,3 @@ formClosebtn.addEventListener('click', () => {
     formModal.close()
 })
 
-const taskCards = document.querySelectorAll('.task-card')
-
-const taskCardDetail = document.createElement('div');
-taskCardDetail.classList.add('task-card-details'); 
-
-const paragraph = document.createElement('p');
-paragraph.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam corrupti soluta quam ipsum, illum doloremque iste nostrum blanditiis excepturi eum dolores fugiat deleniti fuga et consequatur aperiam perspiciatis amet reprehenderit.";
-
-taskCardDetail.appendChild(paragraph);
-
-taskCards.forEach((card) => {
-    card.addEventListener('click', () => {
-        const details = card.querySelector('.task-card-details')
-
-        if (details.style.display === 'none' || details.style.display === '') {
-            details.style.display = 'block'; // Show the details
-        } 
-        else {
-            details.style.display = 'none'; // Hide the details
-        }
-
-    })
-})
